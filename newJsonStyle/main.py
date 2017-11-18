e0aa51f7d096088da26d79a1f755671000884200
Daniel Thomas <danielt998@gmail.com>
2017-11-18 17:49:44 +0000

    added exclude list code

--- newJsonStyle/main.py
+++ newJsonStyle/main.py
@@ -28,9 +28,53 @@ def getLangString(lang_code, dict):
         return ''
     return value
 
+def getCountry(dict):
+    return getValuesFromPropertyID(dict, "P17")
+
+def getValueFromProperty(element):
+    mainsnak = element.get('mainsnak')
+    if mainsnak == None:
+        return ''
+    datavalue = mainsnak.get('datavalue')
+    if datavalue == None:
+        return ''
+    value = datavalue.get('value')
+    if value == None:
+        return ''
+    itemId = value.get('id')
+    if itemId == None:
+        return ''
+    return itemId
+
+LIST_DELIMITER=','
+#return as a list separated by LIST_DELIMITER
+def getRegions(dict):
+    return getValuesFromPropertyID(dict,'P131')
+def getTypes(dict):
+    return getValuesFromPropertyID(dict,'P31')
+def getValuesFromPropertyID(dict, propertyID):
+    items = []
+    if i == None:
+        return ''
+    claimsList = i.get('claims')
+    if claimsList == None:
+        return ''
+    values = claimsList.get(propertyID)
+    if values == None:
+        return ''
+    for element in values:
+        items.append(getValueFromProperty(element))
+    ListString = ""
+    for index, item in enumerate(items):
+        if index==0:
+            ListString=item
+        else:
+            ListString = ListString+LIST_DELIMITER+item
+    return ListString
+
 DELIMITER='\t'
-#FILE='/media/dtm/wikidata/wikidata-20171016-all.json'
-FILE='/media/dtm/wikidata/first1000lines.json'
+FILE='/media/dtm/wikidata/wikidata-20171016-all.json'
+#FILE='/media/dtm/wikidata/first1000lines.json'
 count=0
 langs={'zh','zh-hans','zh-hant','zh-hk','zh-mo','zh-my','zh-sg','zh-tw'}
 
@@ -54,4 +98,4 @@ with open(FILE) as infile:
         if str.isspace():#this only applies while delimiter is a tab **change this**
             continue
         str=str+getLangString('en',i)+DELIMITER
-        print(str+DELIMITER+getDesc('en',i))
+        print(str+DELIMITER+getDesc('en',i)+DELIMITER+getTypes(line)+DELIMITER+getCountry(line)+DELIMITER+getRegions(line))
---
+++ newJsonStyle/remove_excludes.py
@@ -0,0 +1,33 @@
+import sys
+import string
+
+orig_directory=sys.argv[1]
+
+excludeList = None
+with open('excludeList') as f:
+    excludeList = f.readlines()
+#for s in excludeList:
+#    print("EXCLUDED:"+s)
+
+all_types = []
+
+the_list= open(orig_directory)
+for line in the_list:
+    types = string.split(line, '\t')
+    types_list = string.split(types[11], ',')
+    exclude = False
+    for a_type in types_list:
+        #---------
+        if a_type.strip() not in all_types:
+            all_types.append(a_type.strip)
+        #----------
+        for exclude_item in string.split(excludeList,'#')[0]:
+            if a_type.strip() == exclude_item.strip():
+                exclude = True
+    if not exclude:
+        print(line)
+    
+
+
+#for tp in all_types:
+#    print(tp)
